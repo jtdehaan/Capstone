d6755a6ea8972386e038b33a9b47f300b5400fff
Alex Toradze <toradze@indiana.edu>
2018-02-25 17:39:14 -0500

    updated

---
+++ MedConnect/app/src/main/java/com/example/android/medconnect/DoctorPatientListActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.medconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.io.BufferedInputStream;
+
+public class DoctorPatientListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doctor_patient_list);
+
+
+    }
+}
