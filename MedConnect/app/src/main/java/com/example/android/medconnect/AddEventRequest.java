e91af1e54bab09254330f290668f7521b96b5286
Alex Toradze <toradze@indiana.edu>
2018-04-05 21:22:46 -0400

    updating events successfully

--- MedConnect/app/src/main/java/com/example/android/medconnect/AddEventRequest.java
+++ MedConnect/app/src/main/java/com/example/android/medconnect/AddEventRequest.java
@@ -11,7 +11,7 @@ public class AddEventRequest extends StringRequest {
     private static final String ADD_EVENT_REQUEST_URL = "http://cgi.soic.indiana.edu/~team37/Add_Event_APP.php";
     private Map<String, String> params;
 
-    public AddEventRequest(String EventName, String Description, String Location, String Times, String Price, String Date,String ID, Response.Listener<String> listener){
+    public AddEventRequest(String EventName, String Description, String Location, String Times, String Price, String Date, String ID, Response.Listener<String> listener){
         super(Method.POST, ADD_EVENT_REQUEST_URL, listener, null);
         params = new HashMap<>();
         params.put("Eventname", EventName);
